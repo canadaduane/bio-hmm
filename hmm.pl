fa7ecd9334e7aee9eaf785c2784f617b327a98a5
Duane Johnson <duane.johnson@gmail.com>
2009-01-29 10:57:37 -0700

    Fixed get_states, added slider & print_slider

--- hmm.pl
+++ hmm.pl
@@ -18,6 +18,11 @@ use List::Util qw[min max];  #this allows us to use max function
     # An array of states, e.g. 0 for non-CpG and 1 for CpG state
     @state_values = ();
 
+    @cpg = ();
+    @cpg_percentages = ();
+    $threshold = .7;
+    $window = 4;
+    
 # Global Constants:
 
     # Non CpG Island state:
@@ -44,7 +49,7 @@ print "\nBye: " . p_state_given_nucleotide(0, 'a');
 # giving the probabilityOf method the current base and
 # next base?
 sub get_states() {
-    $probability = 1.0;
+    $probability = log(p_initial_state());
     
     # Assume we're not in the middle of an island for starters
     $current_state = $no_cpg_state;
@@ -52,14 +57,16 @@ sub get_states() {
     foreach( 0..@sequence-1 ) {
         $p1 = p_state_given_nucleotide($cpg_state, $sequence[$_]);
         $p2 = p_state_given_nucleotide($no_cpg_state, $sequence[$_]);
-        if( $p1 > $p2 ) {
+        $trans_p1 = p_state_given_state($current_state, $cpg_state);
+        $trans_p2 = p_state_given_state($current_state, $no_cpg_state);
+        if( $p1*$trans_p1 > $p2*$trans_p2 ) {
             $current_state = $cpg_state;
         }
         else {
             $current_state = $no_cpg_state;
         }
         push (@state_values, $current_state);
-        $probability = $probability + log(max($p1, $p2));
+        $probability = $probability + log(max($p1*$trans_p1, $p2*$trans_p2));
     }
 }
 
@@ -71,10 +78,37 @@ sub read_nucleotides($filename) {
     @sequence = split(//,$tmp); # this should split every character
 }
 
-sub slider($window) { #so this method will just check the @state_values
-                     #using a given threshold (the threshold will be a %?
-                     # (percentage will be held in $cpg_island_threshold)
+sub slider {
+    $size = @state_values;
+    $win = shift;
+    $win --;        #this alters the window size for an array (starts at 0)
+    $size = $size-$win; # so if the $size is 100, and $win was 20, we get $size = 79 here
+    for( 0 .. $size ){
+        @tmp = @state_values[$_ .. $win+$_];
+        $total = 0;
+        ($total+=$_) for @tmp;
+        $percent = $total/($win+1);
+        print "position: $_  Total: $total  Percentage: $percent\n";
+        if($percent >= $threshold){
+                push(@cpg, 1);
+        }
+        else {
+                push(@cpg, 0);
+        }
+        push(@cpg_percentage, $percentage);
+    }
 }
+
+sub print_islands() {
+    print @state_values;
+    print "\n";
+    print @cpg;
+    print "\n";
+    print @cpg_percentage;
+    print "\n";
+    print "Threshold: $threshold  Window: $window\n";
+}
+
 
 # this is the "beginning state" probability table
 sub p_initial_state() {
